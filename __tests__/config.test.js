73cdcd255d526bdb13719bf8c19fce08c70a34b4
Joseph Morse <tamed.lionheart@gmail.com>
2019-05-22 11:14:19 -0400

    test(class-properties): added more tests

---
+++ __tests__/config.test.js
@@ -0,0 +1,19 @@
+const { CLIEngine } = require('eslint')
+const fs = require('fs')
+const { resolve } = require('path')
+
+const configFile = resolve(__dirname, '../index.js')
+
+function getErrors(fileToTest) {
+  const cli = new CLIEngine({ configFile })
+
+  return cli.executeOnText(fs.readFileSync(resolve(__dirname, '../lib', fileToTest), 'utf8'))
+}
+
+describe('Validate ESLint config', () => {
+  ['class-property.js'].forEach((file) => {
+    test(`the loaded source file ${file} in ESLint to validate all rules are correct`, () => {
+      expect(getErrors(file).results[0].messages).toEqual([])
+    })
+  })
+})
