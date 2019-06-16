168b10c50477e8fbffc2cd38dad5e12d2c331793
Joseph Morse <tamed.lionheart@gmail.com>
2019-06-16 04:25:27 -0400

    fix(import/no-relative-parent-imports): set to off
    Turning this rule off since I frequently break this rule when working
    with redux.

--- rules/imports.js
+++ rules/imports.js
@@ -86,7 +86,7 @@ module.exports = {
     'import/no-named-export': 'off',
     'import/no-namespace': 'off',
     'import/no-nodejs-modules': 'off',
-    'import/no-relative-parent-imports': 'error',
+    'import/no-relative-parent-imports': 'off',
     'import/no-restricted-paths': 'off',
     'import/no-self-import': 'error',
     'import/no-unassigned-import': 'off',
