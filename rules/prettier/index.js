856fdca4bc9ccb55248afe1b7f6d79c9b1aef9c4
Joseph Morse <tamed.lionheart@gmail.com>
2019-06-20 22:56:21 -0400

    fix(prettier): prettier config in settings
    trying an alternative way to define prettier in my eslint configuration

--- rules/prettier/index.js
+++ rules/prettier/index.js
@@ -3,7 +3,27 @@ const react = require('./react')
 
 const prettier = {
   plugins: ['prettier'],
-  rules: { ...core, ...react, 'prettier/prettier': 'error' }
+  rules: {
+    ...core,
+    ...react,
+    'prettier/prettier': [
+      'error',
+      {
+        arrowParens: 'avoid',
+        bracketSpacing: true,
+        endOfLine: 'lf',
+        jsxBracketSameLine: false,
+        jsxSingleQuote: false,
+        printWidth: 80,
+        quoteProps: 'as-needed',
+        semi: false,
+        singleQuote: true,
+        tabWidth: 2,
+        trailingComma: 'none',
+        useTabs: false
+      }
+    ]
+  }
 }
 
 module.exports = prettier
