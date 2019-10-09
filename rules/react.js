809a6b4b1b147415b8f438dc4590c381de510edf
Joseph Morse <tamed.lionheart@gmail.com>
2019-10-09 07:33:52 -0400

    feat(react/jsx-no-useless-fragment): set to error
    updated dependencies and added a new rule

--- rules/react.js
+++ rules/react.js
@@ -169,6 +169,7 @@ module.exports = {
     ],
     'react/jsx-no-target-blank': 'error',
     'react/jsx-no-undef': 'error',
+    'react/jsx-no-useless-fragment': 'error',
     'react/jsx-one-expression-per-line': [
       'error',
       {
